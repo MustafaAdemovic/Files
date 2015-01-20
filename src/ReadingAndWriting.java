83a707278f05ad6a5a21f13d5e382affa30db6db
Mustafa Ademovic <mustafa.ademovic93@gmail.com>
2015-01-20 21:56:00 +0100

    This path runs on any operating system

---
+++ src/ReadingAndWriting.java
@@ -0,0 +1,30 @@
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+/**
+ * This program use JFileChooser for writing and reading files This path runs on any
+ * operating system
+ * 
+ * @author MustafaAdemovic
+ *
+ */
+
+public class ReadingAndWriting {
+
+	public static void main(String[] args) {
+
+		JFileChooser filePicker = new JFileChooser();
+		String adress = System.getProperty("user.home");
+		String path = adress + File.separator + "Documents" + File.separator
+				+ "workspace";
+		filePicker.setCurrentDirectory(new File(path));
+		int open = filePicker.showOpenDialog(filePicker);
+		if (open != filePicker.APPROVE_OPTION) {
+			filePicker.showOpenDialog(filePicker);
+		}
+		filePicker.showOpenDialog(filePicker);
+
+	}
+
+}
